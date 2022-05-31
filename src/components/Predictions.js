fcbccb4a33073a6db51c4d8af754dec01856be5a
khubaib321 <khubaib.afzal11@gmail.com>
2022-05-31 06:53:34 +0500

    Predictor words display

--- src/components/Predictions.js
+++ src/components/Predictions.js
@@ -1,16 +1,26 @@
-function Predictions({ depth = 1 }) {
+function Predictions({ depth = 1, predictions = ["my", "name", "is"] }) {
   return (
     <>
       <div className="w-full py-2 bg-gray-400 shadow-lg shadow-black" />
-      <div className="p-8 w-4/5 mx-auto flex items-center">
-        <span className="flex h-3 w-3">
-          <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-green-500 opacity-75" />
-          <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500" />
-        </span>
+      <div className="p-8 w-4/5 mx-auto flex flex-col items-center">
         <p className="p-2 text-left">
           Predictions will appear here. Press <span className="bg-gray-300 p-1 rounded">Tab</span> to accept each
           one-by-one. Predicting next <b>{depth}</b> word(s).
         </p>
+        <div className="p-2 w-full flex items-center gap-3">
+          <span className="flex h-3 w-3">
+            <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-green-400 opacity-75" />
+            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-400" />
+          </span>
+          {predictions.map((word, key) => {
+            return (
+              // eslint-disable-next-line react/no-array-index-key
+              <p key={key} className="p-1 text-left bg-gray-300 lowercase rounded">
+                {word}
+              </p>
+            );
+          })}
+        </div>
       </div>
     </>
   );
