203298cee5232fa20d6ffbf7c3f23e116c7efbeb
khubaib321 <khubaib.afzal11@gmail.com>
2022-05-31 12:26:13 +0500

    Minor updates

--- src/components/Notepad.js
+++ src/components/Notepad.js
@@ -2,10 +2,10 @@ function Notepad({ placeholder }) {
   return (
     <div className="p-8 w-4/5 mx-auto">
       <p className="p-2 w-full text-center">
-        Type a few sentences below and see next word predictions appear in the pane below!
+        Type a few sentences and see the next word predictions appear in the section below!
       </p>
       <textarea
-        className="w-full h-40 p-2 bg-black text-white rounded-lg mx-auto"
+        className="w-full h-40 p-2 bg-black text-gray-400 rounded-lg mx-auto"
         placeholder={placeholder || "Type here..."}
       />
     </div>
--- src/components/Predictions.js
+++ src/components/Predictions.js
@@ -5,27 +5,29 @@ function Predictions({ depth = 1 }) {
   return (
     <>
       <div className="w-full py-2 bg-gray-400 shadow-lg shadow-black" />
-      <div className="p-8 w-4/5 mx-auto flex flex-col items-center">
-        <p className="p-2 text-left">
-          Predictions will appear here. Press <span className="bg-gray-300 p-1 rounded">Tab</span> to accept each
-          one-by-one. Predicting next <b>{depth}</b> word(s).
-        </p>
-        <div className="p-2 w-full flex items-center gap-3">
-          <span className="flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-green-400 opacity-75" />
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-400" />
-          </span>
-          {predictions.map((word, key) => (
-            <button
-              // eslint-disable-next-line react/no-array-index-key
-              key={key}
-              type="button"
-              className="p-1 text-left bg-gray-300 lowercase rounded"
-              onClick={() => predictions.shift() && setPredictions([...predictions])}
-            >
-              {word}
-            </button>
-          ))}
+      <div className="w-full bg-gray-500 bg-opacity-20">
+        <div className="p-8 w-4/5 mx-auto flex flex-col items-center">
+          <p className="p-2 text-left">
+            While typing above, press <span className="bg-gray-300 p-1 rounded">Tab</span> to accept the words appearing
+            below one-by-one. Predicting next <b>{depth}</b> word(s)...
+          </p>
+          <div className="p-2 w-full flex items-center gap-3">
+            <span className="flex h-3 w-3">
+              <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-green-500 opacity-75" />
+              <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500" />
+            </span>
+            {predictions.map((word, key) => (
+              <button
+                // eslint-disable-next-line react/no-array-index-key
+                key={key}
+                type="button"
+                className="p-1 text-left bg-gray-300 lowercase rounded"
+                onClick={() => predictions.shift() && setPredictions([...predictions])}
+              >
+                {word}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </>
